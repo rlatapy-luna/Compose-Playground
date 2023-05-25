acc19091bc8a84230a76708e94f97c84478a9368
rlatapy-luna <remi.latapy@lunabee.com>
2023-05-25 18:50:52 +0200

    wip

--- app/src/main/java/rlatapy/composeplayground/MainActivity.kt
+++ app/src/main/java/rlatapy/composeplayground/MainActivity.kt
@@ -3,24 +3,16 @@ package rlatapy.composeplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredSize
-import androidx.compose.foundation.layout.systemBars
-import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
@@ -34,11 +26,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
 import androidx.core.view.WindowCompat
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import kotlinx.coroutines.launch
@@ -58,51 +47,27 @@ class MainActivity : ComponentActivity() {
                 darkIcons = !isSystemInDarkTheme(),
             )
 
-            var showDialog by remember {
-                mutableStateOf(false)
-            }
-
-            var showBottomSheet by remember {
+            var toggle by remember {
                 mutableStateOf(false)
             }
 
             MaterialTheme {
-                if (showDialog) {
-                    Dialog(onDismissRequest = { showDialog = false }) {
-                        Card(
-                            modifier = Modifier.requiredSize(width = 300.dp, height = 100.dp),
-                            shape = MaterialTheme.shapes.extraLarge,
-                            colors = CardDefaults.cardColors(),
-                            elevation = CardDefaults.cardElevation(),
-                            border = null,
-                        ) {
-
+                Column(Modifier.fillMaxSize()) {
+                    Button(onClick = {
+                        toggle = !toggle
+                    }) {
+                        Text(text = "Switch")
+                    }
+                    LazyColumn {
+                        if (toggle) {
+                            item {
+                                Text(text = "text 1", Modifier.fillMaxWidth())
+                            }
+                        } else {
+                            item(key = "key2") {
+                                Text(text = "text 2", Modifier.fillMaxWidth())
+                            }
                         }
-                    }
-                }
-
-                if (showBottomSheet) {
-                    BottomSheetSample(closeBottomSheet = { showBottomSheet = false })
-                }
-
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(Color.Green)
-                        .windowInsetsPadding(WindowInsets.systemBars)
-                        .padding(top = 50.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.spacedBy(16.dp),
-                ) {
-                    Button(onClick = {
-                        showDialog = true
-                    }) {
-                        Text("Show dialog")
-                    }
-                    Button(onClick = {
-                        showBottomSheet = true
-                    }) {
-                        Text("Show bottom sheet")
                     }
                 }
             }
