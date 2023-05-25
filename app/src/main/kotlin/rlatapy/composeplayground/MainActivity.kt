46b15f5929ab1332748f4ffa854830dece2146ba
rlatapy-luna <remi.latapy@lunabee.com>
2023-05-25 18:50:52 +0200

    wip

--- app/src/main/kotlin/rlatapy/composeplayground/MainActivity.kt
+++ app/src/main/kotlin/rlatapy/composeplayground/MainActivity.kt
@@ -1,18 +1,126 @@
 package rlatapy.composeplayground
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.core.view.WindowCompat
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val isDark = (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) ==
+            Configuration.UI_MODE_NIGHT_YES
+        // Don't use SystemBarStyle.auto for navigation bar because it always add a scrim (cf doc)
+        val navigationBarStyle = if (isDark) {
+            SystemBarStyle.dark(scrim = android.graphics.Color.TRANSPARENT)
+        } else {
+            SystemBarStyle.light(scrim = android.graphics.Color.TRANSPARENT, darkScrim = android.graphics.Color.TRANSPARENT)
+        }
+        val statusBarStyle = SystemBarStyle.auto(android.graphics.Color.TRANSPARENT, android.graphics.Color.TRANSPARENT)
+        enableEdgeToEdge(
+            statusBarStyle = statusBarStyle,
+            navigationBarStyle = navigationBarStyle,
+        )
 
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
         setContent {
+            var toggle by remember {
+                mutableStateOf(false)
+            }
 
+            MaterialTheme {
+                Column(Modifier.fillMaxSize()) {
+                    Button(onClick = {
+                        toggle = !toggle
+                    }) {
+                        Text(text = "Switch")
+                    }
+                    LazyColumn {
+                        if (toggle) {
+                            item {
+                                Text(text = "text 1", Modifier.fillMaxWidth())
+                            }
+                        } else {
+                            item(key = "key2") {
+                                Text(text = "text 2", Modifier.fillMaxWidth())
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BottomSheetSample(
+    closeBottomSheet: () -> Unit,
+) {
+    val scope = rememberCoroutineScope()
+    val bottomSheetState = rememberModalBottomSheetState(
+        skipPartiallyExpanded = false,
+    )
+
+    ModalBottomSheet(
+        onDismissRequest = closeBottomSheet,
+        sheetState = bottomSheetState,
+    ) {
+        Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+            Button(
+                // Note: If you provide logic outside of onDismissRequest to remove the sheet,
+                // you must additionally handle intended state cleanup, if any.
+                onClick = {
+                    scope.launch { bottomSheetState.hide() }.invokeOnCompletion {
+                        if (!bottomSheetState.isVisible) {
+                            closeBottomSheet()
+                        }
+                    }
+                }
+            ) {
+                Text("Hide Bottom Sheet")
+            }
+        }
+        LazyColumn {
+            items(50) {
+                ListItem(
+                    headlineContent = { Text("Item $it") },
+                    leadingContent = {
+                        Icon(
+                            Icons.Default.Favorite,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                )
+            }
         }
     }
 }
