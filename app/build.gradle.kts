636b6c8ad8547c4dfd7f3ae143f0d153821b2d6a
rlatapy-luna <remi.latapy@lunabee.com>
2023-05-25 23:35:06 +0200

    kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,64 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "rlatapy.composeplayground"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "rlatapy.composeplayground"
+        minSdk = 30
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "benchmark-proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "_"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:_")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:_")
+    implementation("androidx.activity:activity-compose:_")
+
+    api(platform("dev.chrisbanes.compose:compose-bom:_"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+
+    implementation("com.google.accompanist:accompanist-systemuicontroller:_")
+
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,12 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
+plugins {
+    // See https://jmfayard.github.io/refreshVersions
+    id("de.fayard.refreshVersions") version "0.51.0"
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -13,4 +19,4 @@ dependencyResolutionManagement {
     }
 }
 rootProject.name = "ComposePlayground"
-include ':app'
+include(":app")
