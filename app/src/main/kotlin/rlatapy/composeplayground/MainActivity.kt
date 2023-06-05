bd94a5e512c59fdc71a01064eb4e9a6dfd64c0e2
rlatapy-luna <remi.latapy@lunabee.com>
2023-06-05 11:04:43 +0200

    Update to use WindowInsets

--- app/src/main/kotlin/rlatapy/composeplayground/MainActivity.kt
+++ app/src/main/kotlin/rlatapy/composeplayground/MainActivity.kt
@@ -10,13 +10,16 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.windowInsetsPadding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -100,6 +103,7 @@ fun BottomSheetSample(
     ModalBottomSheet(
         onDismissRequest = closeBottomSheet,
         sheetState = bottomSheetState,
+        windowInsets = WindowInsets(0),
     ) {
         Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
             Button(
@@ -116,16 +120,18 @@ fun BottomSheetSample(
                 Text("Hide Bottom Sheet")
             }
         }
-        LazyColumn {
-            item {
-                TextField(
-                    value = textValue,
-                    onValueChange = { textValue = it },
-                    modifier = Modifier
-                        .padding(16.dp)
-                        .fillMaxWidth(),
-                )
-            }
-        }
+        TextField(
+            value = textValue,
+            onValueChange = { textValue = it },
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+        )
+        Spacer(
+            Modifier
+                .background(Color.Cyan)
+                .fillMaxWidth()
+                .windowInsetsPadding(WindowInsets.systemBars.only(WindowInsetsSides.Bottom))
+        )
     }
 }
