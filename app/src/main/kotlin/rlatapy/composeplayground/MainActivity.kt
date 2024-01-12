0a1afca60856e7dd2b42eb12bbae27167722e70a
rlatapy-luna <remi.latapy@lunabee.com>
2024-01-12 14:04:27 +0100

    Add working repro beta01

--- app/src/main/kotlin/rlatapy/composeplayground/MainActivity.kt
+++ app/src/main/kotlin/rlatapy/composeplayground/MainActivity.kt
@@ -1,8 +1,28 @@
 package rlatapy.composeplayground
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
 
 class MainActivity : ComponentActivity() {
@@ -12,7 +32,76 @@ class MainActivity : ComponentActivity() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
         setContent {
-
+            MainContent()
         }
     }
+
+}
+
+@Composable
+fun MainContent() {
+    MaterialTheme(
+        colorScheme = MaterialTheme.colorScheme.copy(
+            surface = Color.Green,
+            onSurface = Color.Blue,
+            error = Color.Red,
+            onError = Color.Yellow
+        )
+    ) {
+        Column(
+            Modifier
+                .fillMaxWidth()
+                .systemBarsPadding()
+                .padding(horizontal = 16.dp)
+                .verticalScroll(rememberScrollState())
+        ) {
+            Spacer(modifier = Modifier.height(16.dp))
+            Card {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(50.dp),
+                    contentAlignment = Alignment.Center,
+                ) {
+                    Text("Default colors")
+                }
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Card(
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.surface
+                )
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(50.dp),
+                    contentAlignment = Alignment.Center,
+                ) {
+                    Text("Force surface (green) container\nExpect blue text")
+                }
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Card(
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.error
+                )
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(50.dp),
+                    contentAlignment = Alignment.Center,
+                ) {
+                    Text("Force error (red) container\nExpect yellow text")
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun MainContentPreview() {
+    MainContent()
 }
