c5118217bbcb1a94c24bf875703ac57b27680f6b
rlatapy-luna <remi.latapy@lunabee.com>
2025-01-14 23:36:15 +0100

    sdk 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "rlatapy.composeplayground"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "rlatapy.composeplayground"
         minSdk = 30
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
